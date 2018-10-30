92f3f86614abb3d5d541922a22bf5c225795ee44
huarui <huarui.wu@gmail.com>
2018-10-30 02:02:36 -0700

    show week menu

--- dist.js
+++ dist.js
@@ -1,31 +1,92 @@
-const { h, render, Component, Color } = require('ink');
+const {
+    h,
+    render,
+    Component,
+    Color,
+    Fragment,
+    Bold,
+    span,
+    div
+} = require('ink');
 const fetch = require('node-fetch');
+const addDays = require('date-fns/add_days');
+const format = require('date-fns/format');
 
 const ZESTY_ID = process.env.ZESTY_ID;
 const ZESTY_ENDPOINT = 'https://api.zesty.com/client_portal_api/meals';
 
+const getMealsByDate = (meals, startDate, endDate) => meals.filter(meal => {
+    const date = new Date(meal.delivery_date);
+    return date >= startDate && date <= endDate;
+});
+
 class WeekTable extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            meals: []
+        };
     }
 
     componentWillMount() {
         const { zestyId } = this.props;
-        fetch(ZESTY_ENDPOINT + '?client_id=' + zestyId).then(res => res.json()).then(console.log);
+        fetch(ZESTY_ENDPOINT + '?client_id=' + zestyId).then(res => res.json()).then(res => this.setState({
+            meals: res.meals
+        }));
     }
 
     render() {
+        const { meals } = this.state;
+        const currentDate = new Date();
+        const mealsOfWeek = getMealsByDate(meals, currentDate, addDays(currentDate, 7));
         return h(
-            Color,
-            { green: true },
-            this.state.i,
-            ' tests passed'
+            Fragment,
+            null,
+            mealsOfWeek.map(meal => {
+                return h(
+                    Fragment,
+                    null,
+                    h(
+                        'div',
+                        null,
+                        h(
+                            Bold,
+                            null,
+                            format(meal.delivery_date, 'ddd')
+                        ),
+                        h(
+                            'span',
+                            null,
+                            format(meal.delivery_date, 'MMM DD, YYYY'),
+                            ' ',
+                            '|',
+                            ' '
+                        ),
+                        h(
+                            Color,
+                            { green: true },
+                            meal.restaurant_name
+                        ),
+                        h(
+                            Color,
+                            { blue: true },
+                            ' [',
+                            meal.restaurant_cuisine,
+                            ']'
+                        )
+                    ),
+                    h(
+                        'div',
+                        null,
+                        h(
+                            'span',
+                            null,
+                            format(meal.delivery_date, 'h:ma')
+                        )
+                    )
+                );
+            })
         );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer);
     }
 }
 
--- index.js
+++ index.js
@@ -1,30 +1,77 @@
-const { h, render, Component, Color } = require('ink')
+const {
+    h,
+    render,
+    Component,
+    Color,
+    Fragment,
+    Bold,
+    span,
+    div
+} = require('ink')
 const fetch = require('node-fetch')
+const addDays = require('date-fns/add_days')
+const format = require('date-fns/format')
 
 const ZESTY_ID = process.env.ZESTY_ID
 const ZESTY_ENDPOINT = 'https://api.zesty.com/client_portal_api/meals'
 
-// const getMealsByDate(meals, startDay, endDay)
+const getMealsByDate = (meals, startDate, endDate) =>
+    meals.filter(meal => {
+        const date = new Date(meal.delivery_date)
+        return date >= startDate && date <= endDate
+    })
 
 class WeekTable extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            meals: []
+        }
     }
 
     componentWillMount() {
         const { zestyId } = this.props
         fetch(ZESTY_ENDPOINT + '?client_id=' + zestyId)
             .then(res => res.json())
-            .then(console.log)
+            .then(res =>
+                this.setState({
+                    meals: res.meals
+                })
+            )
     }
 
     render() {
-        return <Color green>{this.state.i} tests passed</Color>
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer)
+        const { meals } = this.state
+        const currentDate = new Date()
+        const mealsOfWeek = getMealsByDate(
+            meals,
+            currentDate,
+            addDays(currentDate, 7)
+        )
+        return (
+            <Fragment>
+                {mealsOfWeek.map(meal => {
+                    return (
+                        <Fragment>
+                            <div>
+                                <Bold>{format(meal.delivery_date, 'ddd')}</Bold>
+                                <span>
+                                    {format(meal.delivery_date, 'MMM DD, YYYY')}{' '}
+                                    |{' '}
+                                </span>
+                                <Color green>{meal.restaurant_name}</Color>
+                                <Color blue> [{meal.restaurant_cuisine}]</Color>
+                            </div>
+                            <div>
+                                <span>
+                                    {format(meal.delivery_date, 'h:ma')}
+                                </span>
+                            </div>
+                        </Fragment>
+                    )
+                })}
+            </Fragment>
+        )
     }
 }
 
